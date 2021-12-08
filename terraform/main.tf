a086f5680bc1eb28053b4000a9c0a1a9877c7f4a
Eduard Skuratov <skuratov.eduard.dim@gmail.com>
2021-12-08 16:42:09 +0000

    Add http p 80

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,9 +5,19 @@ provider "aws" {
 resource "aws_instance" "test" {
   ami           = "ami-020db2c14939a8efb"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id] 
   tags = {
     Name = "server"
   }
 }
 
+resource "aws_security_group" "instance" {
+  name = "terraform-test-instance"
 
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
