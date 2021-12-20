3dfba8ac1bdde8bb0818143d3c762f0cd5ae40e4
SkuratovEduard <skuratov.eduard.dim@gmail.com>
2021-12-20 17:03:24 +0000

    add nginx

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,17 +2,18 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_instance" "test" {
-  ami           = "ami-020db2c14939a8efb"
+resource "aws_instance" "web" {
+  ami           = "ami-03a0c45ebc70f98ea"
   instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id] 
+  vpc_security_group_ids = [aws_security_group.instance.id]
+  user_data = file("user_data.sh")
   tags = {
     Name = "server"
   }
 }
 
 resource "aws_security_group" "instance" {
-  name = "terraform-test-instance"
+  name = "terraform-web-instance"
 
   ingress {
     from_port   = 80
